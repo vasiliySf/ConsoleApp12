787492165c2bb0193ae9036c3f49189c23aca212
vasiliySf <vasiliy.ts@gmail.com>
2022-05-25 23:38:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp12;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<User> users = new List<User>();
+        string login;
+        User muser = new User();
+
+        users.Add(new User("Petrov", "Петров Петр Петрович", false));
+        users.Add(new User("Ivanov", "Иванов Иван Иванович", true));
+        users.Add(new User("Smirnov", "Смирнов Игорь Сергеевич", false));
+        try
+        {
+            Console.WriteLine("Введите логин!");
+            login = Console.ReadLine();
+
+            foreach (User user in users)
+            {
+                if (user.Login == login)
+                {
+                    muser = user;
+                    break;
+                }
+            }
+            Console.WriteLine($"Привет, {muser.Name}! ");
+
+            if (!muser.IsPremium)
+            {
+                muser.ShowAds();
+            }
+        }
+        catch (Exception ex)
+        { Console.WriteLine(ex.ToString()); }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ConsoleApp12
+{
+    internal class User
+    {
+           public string Login { get; set; }
+            public string Name { get; set; }
+            public bool IsPremium { get; set; }
+        public User()
+        {
+
+        }
+        public User(string Login, string Name, bool IsPremium)
+        {
+             this.Login = Login;
+             this.Name = Name;
+             this.IsPremium = IsPremium;
+        }
+        public void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+    
+}
